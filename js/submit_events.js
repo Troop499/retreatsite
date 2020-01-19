ea5e09f38b8ac6f2d00e48cef1b1970f770a8b3b
Mohammed Abdulhaadi Rayyan Ansari <mohdhaadiansari@gmail.com>
2020-01-18 19:26:45 -0800

    testing out SMTPJS

---
+++ js/submit_events.js
@@ -0,0 +1,11 @@
+sendEmail() {
+  Email.send({
+    SecureToken : "7bc19e23-4746-4446-97fc-494cc2508714",
+    To :  document.getElementById("email").value; ,
+    From : "troop499dev@gmail.com",
+    Subject : "test - Troop 499 Retreat",
+    Body : "testing testing testing123"
+}).then(
+  message => alert(message)
+);
+}
