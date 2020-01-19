13b055bd4ca4bf802fabff6aee49c0dee71b157a
Mohammed Abdulhaadi Rayyan Ansari <mohdhaadiansari@gmail.com>
2020-01-18 21:28:22 -0800

    hopefully SMTPJS will work this time... hopefully

--- js/submit_events.js
+++ js/submit_events.js
@@ -1,11 +1,12 @@
 function sendEmail() {
   Email.send({
-    SecureToken : "7bc19e23-4746-4446-97fc-494cc2508714",
-    To :  document.getElementById("email").value ,
+    SecureToken : "159f6b99-b22f-44b0-a581-1f02bf901018",
+    To :  'abdulhaadiansari@gmail.com',
     From : "troop499dev@gmail.com",
     Subject : "test - Troop 499 Retreat",
     Body : "testing testing testing123"
 }).then(
   message => alert(message)
 );
+console.log(document.getElementById("email").value); // for debugging purposes
 }
