fa313383cb75df8543f9aebf916bbb5ff92e2315
ansarirayyan <29453512+ansarirayyan@users.noreply.github.com>
2020-01-26 17:30:32 -0800

    tried to fix the response that node.js is supposed to give

--- api/submit_events.js
+++ api/submit_events.js
@@ -34,30 +34,19 @@ module.exports = async (req, res) => {
 
       transporter.sendMail(message, (err, info) => {
           if (err) {
-              console.log('Error occurred. ' + err.message);
-
-              let response = await fetch(err.message, {
-			             headers: {
-				                 "Authorization": `Basic ${basic}`
-			                    }
-		          });
-		          res.status(response.status);
-		          res.end(await response.text());
+              res.end(err.message);
 
               return process.exit(1);
           }
 
-          let response = await fetch(nodemailer.getTestMessageUrl(info)), {
-               headers: {
-                     "Authorization": `Basic ${basic}`
-            }
-
-            res.status(response.status);
-            res.end(await response.text());
+          res.end(nodemailer.getTestMessageUrl(info)));
 
           console.log('Message sent: %s', info.messageId);
           // Preview only available when sending through an Ethereal account
           console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+          
+          res.status(response.status);
+		      res.end(await response.text());
       });
   });
 
